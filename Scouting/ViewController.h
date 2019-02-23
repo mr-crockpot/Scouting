4a061f76c18ed8044bb63ea07385392f07bbc83e
mr-crockpot <adamschor@adams-macbook-air.local>
2019-02-22 19:46:29 -0700

    Initial Commit

---
+++ Scouting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scouting
+//
+//  Created by Adam Schor on 2/22/19.
+//  Copyright © 2019 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Scouting/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Scouting
+//
+//  Created by Adam Schor on 2/22/19.
+//  Copyright © 2019 AandA Development. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
